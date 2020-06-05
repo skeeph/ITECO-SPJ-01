c7d32a833bdd849bfb40d3da9b560becf8708a99
Khabib Murtuzaaliev <murtuzaaliev@i-teco.ru>
2020-06-05 09:51:23 +0300

    Initial commit

---
+++ src/main/java/ru/volnenko/se/config/AppConfiguration.java
@@ -0,0 +1,7 @@
+package ru.volnenko.se.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfiguration {
+}
---
+++ src/main/java/ru/volnenko/se/service/DomainService.java
@@ -0,0 +1,40 @@
+package ru.volnenko.se.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.volnenko.se.api.service.IDomainService;
+import ru.volnenko.se.api.service.IProjectService;
+import ru.volnenko.se.api.service.ITaskService;
+import ru.volnenko.se.api.service.ServiceLocator;
+import ru.volnenko.se.entity.Domain;
+
+/**
+ * @author Denis Volnenko
+ */
+@Service
+public final class DomainService implements IDomainService {
+    private final IProjectService projectService;
+    private final ITaskService taskService;
+
+    @Autowired
+    public DomainService(final IProjectService projectService, final ITaskService taskService) {
+        this.projectService = projectService;
+        this.taskService = taskService;
+        
+    }
+
+    @Override
+    public void load(final Domain domain) {
+        if (domain == null) return;
+        projectService.load(domain.getProjects());
+        taskService.load(domain.getTasks());
+    }
+
+    @Override
+    public void export(final Domain domain) {
+        if (domain == null) return;
+        domain.setProjects(projectService.getListProject());
+        domain.setTasks(taskService.getListTask());
+    }
+
+}
